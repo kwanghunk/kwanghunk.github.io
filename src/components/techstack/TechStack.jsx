858d9ebfd1804c68e3b69390c74741b04b015604
kwanghunk <hosookkh@gmail.com>
2025-02-01 14:10:30 +0900

    Update browser icon And title

--- src/components/techstack/TechStack.jsx
+++ src/components/techstack/TechStack.jsx
@@ -7,12 +7,14 @@ const TechStack = () => {
   return (
     <Container className="techstack-container">
       <h2 className="section-title">{skills.title}</h2>
-      {skills.mySkills.map((category, index) => (
+      
+      {/* CORE and FRONT */}
+      {skills.mySkills.slice(0, 2).map((category, index) => (
         <div key={index} className="tech-category">
           <h3 className="category-title">- {category.subject} -</h3>
           <Row className="tech-icons">
             {category.skills.map((skill, idx) => (
-              <Col key={idx} xs={6} sm={4} md={3} lg={2} className="tech-col">
+              <Col key={skill} xs={6} sm={4} md={3} lg={2} className="tech-col">
                 <div className="tech-item">
                   <img
                     src={require(`../../asset/img/${skill}.svg`)}
@@ -26,6 +28,48 @@ const TechStack = () => {
           </Row>
         </div>
       ))}
+
+      {/* SERVER and RDBMS */}
+      <div className="tech-category">
+        <h3 className="category-title">- SERVER & RDBMS -</h3>
+        <Row className="tech-icons">
+          {skills.mySkills.slice(2, 4).flatMap((category) =>
+            category.skills.map((skill, idx) => (
+              <Col key={skill} xs={6} sm={4} md={3} lg={2} className="tech-col">
+                <div className="tech-item">
+                  <img
+                    src={require(`../../asset/img/${skill}.svg`)}
+                    alt={skill}
+                    className="tech-icon"
+                  />
+                  <p className="tech-name">{skill}</p>
+                </div>
+              </Col>
+            ))
+          )}
+        </Row>
+      </div>
+
+      {/* IDE and OTHER */}
+      <div className="tech-category">
+        <h3 className="category-title">- IDE & OTHER -</h3>
+        <Row className="tech-icons">
+          {skills.mySkills.slice(4, 6).flatMap((category) =>
+            category.skills.map((skill, idx) => (
+              <Col key={skill} xs={6} sm={4} md={3} lg={2} className="tech-col">
+                <div className="tech-item">
+                  <img
+                    src={require(`../../asset/img/${skill}.svg`)}
+                    alt={skill}
+                    className="tech-icon"
+                  />
+                  <p className="tech-name">{skill}</p>
+                </div>
+              </Col>
+            ))
+          )}
+        </Row>
+      </div>
     </Container>
   );
 };
